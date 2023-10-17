795bef144bed76fd003feb5bd00371de4182ba55
kyuyeol.han <kyuyeol.han@42dot.ai>
2023-10-17 16:50:36 +0900

    sync

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import {useState} from "react";
+import AppRouter from "components/Router";
+import {auth} from "../fbase";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  console.log(auth.currentUser)
+  return (
+      <>
+        <AppRouter isLoggedIn={isLoggedIn} />
+        <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+      </>
+  );
+}
+
+export default App;
---
+++ src/components/Router.js
@@ -0,0 +1,21 @@
+import {HashRouter as Router, Route, Routes} from "react-router-dom";
+import Home from "routes/Home";
+import Auth from "routes/Auth";
+
+const AppRouter = ({isLoggedIn}) => {
+    return (
+        <Router>
+            <Routes>
+                {
+                    isLoggedIn ? (
+                        <Route exact path="/" element={<Home/>}/>
+                    ) : (
+                        <Route exact path="/" element={<Auth />}/>
+                    )
+                }
+            </Routes>
+        </Router>
+    );
+}
+
+export default AppRouter;
